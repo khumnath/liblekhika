e46a5b890670ca4558a665f74604cef5f344914d
khumnath cg <nath.khum@gmail.com>
2025-10-06 18:15:59 +0545

    included unorder map

--- core/src/lekhika_core.cpp
+++ core/src/lekhika_core.cpp
@@ -24,6 +24,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>
 #include <cstring>
 #include <filesystem>
 #include <stdexcept>
+#include <unordered_map>
 
 // ICU includes for Unicode string handling and validation
 #include <unicode/unistr.h>
